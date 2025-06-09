3ec3b29c60e71118127e1a545603d194b1fa368a
윤미수 <ymisu9@gmail.com>
2025-06-09 23:00:47 +0900

    [Bronze II] Title: 나머지, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    int input, result=0;
+    int remain[10];
+    
+    for(int i=0; i<10; i++) {
+        scanf("%d", &input);
+        remain[i] = (input % 42);
+    }
+    
+    for(int i=0; i<10; i++) {
+        int count=0; 
+        for(int j=i+1; j<10; j++) { 
+            if(remain[i] == remain[j]) count++;
+        }
+        if (count == 0) result++; 
+    }
+    
+   printf("%d", result);
+}
