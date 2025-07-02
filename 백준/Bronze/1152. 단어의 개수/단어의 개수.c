4e964cefb34080b6536b874a4350a02e0fdd538b
윤미수 <ymisu9@gmail.com>
2025-07-02 20:36:03 +0900

    [Bronze II] Title: 단어의 개수, Time: 8 ms, Memory: 2092 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+char s[1000001];
+int main() {
+    int res = 1;
+    scanf("%[^\n]", s);
+	if (strlen(s) == 1 && s[0] == ' ') {
+		printf("0");
+		return 0;
+	}
+	for (int i = 1; i < strlen(s) - 1; i++) {
+		if (s[i] == ' ') res++;
+	}
+	printf("%d", res);
+	return 0;
+}
