88567d4e9d2a0d6e5eaafaaf49a322bf27aa14b4
윤미수 <ymisu9@gmail.com>
2025-07-17 01:25:11 +0900

    [Silver II] Title: 연속합, Time: 92 ms, Memory: 39096 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,8 @@
+n=int(input())
+arr=list(map(int,input().split()))
+max_sum=arr[0]
+current_sum=arr[0]
+for i in range (1, n):
+    current_sum=max(arr[i], current_sum+arr[i])
+    max_sum=max(max_sum, current_sum)
+print(max_sum)
