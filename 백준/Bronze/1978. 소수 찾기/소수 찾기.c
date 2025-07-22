9a682f5881174c2226a1baeed912350b76c849ab
윤미수 <ymisu9@gmail.com>
2025-07-22 18:04:47 +0900

    [Bronze II] Title: 소수 찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[1000];
+    int count=0;
+    for (int i = 0; i < n; i++){
+        scanf("%d",&arr[i]);
+    }
+    for (int i = 0; i < n; i++){
+        if (arr[i] < 2) continue;
+        
+        int is_prime = 1;
+        for (int j = 2; j*j <= arr[i]; j++){
+            if (arr[i] % j == 0){
+                is_prime = 0;
+                break;
+            }
+        }
+        if (is_prime) count++;
+    }
+    printf("%d\n",count);
+    return 0;
+}
