072580b7eaaf3e1cce6ecb07d25b74efaeb4e12d
윤미수 <ymisu9@gmail.com>
2025-08-03 00:53:54 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n, count = 0;
+    scanf("%d", &n);
+
+    while (n >= 5) {
+        count += n / 5;
+        n /= 5;
+    }
+
+    printf("%d", count);
+    return 0;
+}
