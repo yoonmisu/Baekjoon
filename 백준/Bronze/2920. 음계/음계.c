21415cfa3a5bfd0e16f58fed3de14f30fececb86
윤미수 <ymisu9@gmail.com>
2025-08-05 18:06:49 +0900

    [Bronze II] Title: 음계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.c
+++ 백준/Bronze/2920. 음계/음계.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
-int main() {
+int main(){
     int arr[8];
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < 8; i++){
         scanf("%d", &arr[i]);
     }
     int ascending = 1;
     int descending = 1;
-    for (int i = 0; i < 7; i++) {
-        if (arr[i] + 1 != arr[i + 1]) {
+    for (int i = 0; i < 7; i++){
+        if (arr[i] + 1 != arr[i+1])
             ascending = 0;
-        }
-        if (arr[i] - 1 != arr[i + 1]) {
+        if (arr[i] -1 != arr[i+1])
             descending = 0;
-        }
     }
-    if (ascending) {
-        printf("ascending\n");
-    } else if (descending) {
-        printf("descending\n");
-    } else {
-        printf("mixed\n");
-    }
+    if (ascending)
+        printf("ascending");
+    else if (descending)
+        printf("descending");
+    else
+        printf("mixed");
     return 0;
 }
+    
