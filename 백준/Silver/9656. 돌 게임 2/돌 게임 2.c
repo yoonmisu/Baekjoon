9a62650524331f00cc26c25a3c79b13586efd776
윤미수 <ymisu9@gmail.com>
2025-08-11 00:59:14 +0900

    [Silver V] Title: 돌 게임 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    if (N % 2 == 0) {
+        printf("SK\n");
+    } else {
+        printf("CY\n");
+    }
+
+    return 0;
+}
