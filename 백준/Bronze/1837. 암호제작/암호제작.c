ead435640914c893d24c31a491ca412ff7349ab8
윤미수 <ymisu9@gmail.com>
2025-08-13 13:51:03 +0900

    [Bronze III] Title: 암호제작, Time: 868 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1837. 암호제작/암호제작.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int f(char P[], int K){
+    int r = 0;
+    for (int i = 0; P[i] != '\0'; i++){
+        r  = (r * 10 + (P[i] - '0')) % K;
+    }
+    return r;
+}
+int main(){
+    char P[1000];
+    int K;
+    scanf("%s %d", P, &K);
+    for (int i = 2; i < K; i++){
+        if (f(P, i) == 0){
+            printf("BAD %d", i);
+            return 0;
+        }
+    }
+    printf("GOOD");
+    return 0;
+}
