6a18da7b40490fe7c646c4fdb355ec9fbef9b404
윤미수 <ymisu9@gmail.com>
2025-08-16 01:46:33 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++){
+        for (int j = 0; j < n - i; j++)
+            printf(" ");
+        for (int k = 0; k < 2 * i - 1; k++)
+            printf("*");
+        printf("\n");
+    }
+    for (int i = n - 1; i >= 1; i--){
+        for (int j = 0; j < n - i; j++)
+            printf(" ");
+        for (int k = 0; k < 2 * i -1; k++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
